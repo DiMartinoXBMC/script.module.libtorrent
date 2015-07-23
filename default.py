cea4adbd4130b0c6a6b1b31af9600b51e90f371e
DiMartinoXBMC <reportonme@mail.ru>
2015-07-23 22:13:47 +0300

    Edit custom library path with keyboard

--- default.py
+++ default.py
@@ -2,6 +2,7 @@
 
 from python_libtorrent import get_libtorrent, get_platform, log
 import xbmcgui
+import xbmcaddon, xbmc
 
 sucsess=False
 version=''
@@ -20,3 +21,13 @@ except Exception, e:
 
 line2='python-libtorrent %s IMPORTED successfully' % version if sucsess else 'Failed to import python-libtorrent!'
 dialog.ok('Libtorrent','OS:'+p['os']+' arch:'+p['arch'], line2)
+
+__settings__ = xbmcaddon.Addon(id='script.module.libtorrent')
+__language__ = __settings__.getLocalizedString
+if __settings__.getSetting('ask_dirname')=='true':
+    set_dirname=__settings__.getSetting('dirname')
+    keyboard = xbmc.Keyboard(set_dirname, __language__(1002))
+    keyboard.doModal()
+    path_keyboard = keyboard.getText()
+    if path_keyboard and keyboard.isConfirmed():
+        __settings__.setSetting('dirname', path_keyboard)
