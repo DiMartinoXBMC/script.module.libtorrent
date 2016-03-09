0032f49ab9692ba7e560f72312f9f1c4358b4c26
DiMartinoXBMC <reportonme@mail.ru>
2016-03-10 00:03:34 +0300

    kodi 17 fix

--- python_libtorrent/__init__.py
+++ python_libtorrent/__init__.py
@@ -22,37 +22,3 @@
     OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
     WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
-
-from python_libtorrent.python_libtorrent import get_libtorrent, log
-from python_libtorrent.platform_pulsar import get_platform
-import xbmcgui
-import xbmcaddon, xbmc
-
-sucsess=False
-version=''
-p=get_platform()
-dialog = xbmcgui.Dialog()
-
-try:
-    libtorrent=get_libtorrent()
-
-    log('Imported libtorrent v' + libtorrent.version + ' from get_libtorrent()')
-    version=str(libtorrent.version)
-    sucsess=True
-except Exception, e:
-    log('Error importing from get_libtorrent(). Exception: ' + str(e))
-
-
-line2='python-libtorrent %s IMPORTED successfully' % version if sucsess else 'Failed to import python-libtorrent!'
-dialog.ok('Libtorrent','OS:'+p['os']+' arch:'+p['arch'], line2)
-
-__settings__ = xbmcaddon.Addon(id='script.module.libtorrent')
-__language__ = __settings__.getLocalizedString
-if __settings__.getSetting('ask_dirname')=='true':
-    set_dirname=__settings__.getSetting('dirname')
-    __settings__.setSetting('ask_dirname','false')
-    keyboard = xbmc.Keyboard(set_dirname, __language__(1002))
-    keyboard.doModal()
-    path_keyboard = keyboard.getText()
-    if path_keyboard and keyboard.isConfirmed():
-        __settings__.setSetting('dirname', path_keyboard)
